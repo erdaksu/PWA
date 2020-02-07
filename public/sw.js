6a70499e32a30d3d5487e62337fb831bdcf591b7
erdaksu <erdemaksu77@gmail.com>
2020-02-07 04:51:40 +0000

    trying to fix sw

--- public/sw.js
+++ public/sw.js
@@ -1,106 +1,16 @@
-var courses = [
-	{
-		slug: 'mathshendon',
-		topic: 'Maths Classes',
-		location: 'Hendon',
-		price: '£100',
-	},
-	{
-		slug: 'mathscolindale',
-		topic: 'Maths Classes',
-		location: 'Colindale',
-		price: '£110',
-	},
-	{
-		slug: 'mathsbrentcross',
-		topic: 'Maths Classes',
-		location: 'BrentCross',
-		price: '£90',
-	},
-	{
-		slug: 'mathsgoldersgreen',
-		topic: 'Maths Classes',
-		location: 'GoldersGreen',
-		price: '£80',
-	},
-
-	{
-		slug: 'englishhendon',
-		topic: 'English Classes',
-		location: 'Hendon',
-		price: '£70',
-	},
-	{
-		slug: 'englishcolindale',
-		topic: 'English Classes',
-		location: 'Colindale',
-		price: '£70',
-	},
-	{
-		slug: 'englishbrentcross',
-		topic: 'English Classes',
-		location: 'BrentCross',
-		price: '£90',
-	},
-	{
-		slug: 'englishgoldersgreen',
-		topic: 'English Classes',
-		location: 'GoldersGreen',
-		price: '£80',
-	},
-
-	{
-		slug: 'sciencehendon',
-		topic: 'Science Classes',
-		location: 'Hendon',
-		price: '£90',
-	},
-	{
-		slug: 'sciencecolindale',
-		topic: 'Science Classes',
-		location: 'Colindale',
-		price: '£70',
-	},
-	{
-		slug: 'sciencebrentcross',
-		topic: 'Science Classes',
-		location: 'BrentCross',
-		price: '£70',
-	},
-	{
-		slug: 'sciencegoldersgreen',
-		topic: 'Science Classes',
-		location: 'GoldersGreen',
-		price: '£80',
-	},
-
-	{
-		slug: 'sportshendon',
-		topic: 'Sports Classes',
-		location: 'Hendon',
-		price: '£50',
-	},
-	{
-		slug: 'sportscolindale',
-		topic: 'Sports Classes',
-		location: 'Colindale',
-		price: '£35',
-	},
-	{
-		slug: 'sportsbrentcross',
-		topic: 'Sports Classes',
-		location: 'BrentCross',
-		price: '£40',
-	},
-	{
-		slug: 'sportsgoldersgreen',
-		topic: 'Sports Classes',
-		location: 'GoldersGreen',
-		price: '£50',
-	},
-];
+// Installing Service Worker
+self.addEventListener('install', function(e) {
+	console.log('[Service Worker] Install');
+	e.waitUntil(
+	  caches.open(cacheName).then(function(cache) {
+		console.log('[Service Worker] Caching all: app shell and content');
+		return cache.addAll(contentToCache);
+	  })
+	);
+  });
+  
 // Files to cache
-var cacheName = 'PWA-v1';
+var cacheName = 'PWA';
 var appShellFiles = [
   '/PWA/public/',
   '/PWA/public/index.html',
@@ -120,36 +30,3 @@ var appShellFiles = [
   '/PWA/public/icons/icon-512.png',
   '/PWA/public/img/logo.png',
 ];
-
-var coursesImages = [];
-for(var i=0; i<courses.length; i++) {
-  coursesImages.push('data/img/'+courses[i].slug+'.png');
-}
-var contentToCache = appShellFiles.concat(coursesImages);
-
-// Installing Service Worker
-self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(contentToCache);
-    })
-  );
-});
-
-// Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
