dba6d99570929322a08744c381a299cc3d2b88b9
erdaksu <erdemaksu77@gmail.com>
2020-02-07 05:03:50 +0000

    update

--- public/sw.js
+++ public/sw.js
@@ -1,50 +1,32 @@
-// Files to cache
-var cacheName = 'V1';
-var appShellFiles = [
-	'/PWA/public/',
-	'/PWA/public/index.html',
-	'/PWA/public/style.css',
-	'/PWA/public/app.js',
-	'/PWA/public/favicon.ico',
-	'/PWA/public/manifest.webmanifest',
-	'/PWA/public/data/img',
-	'/PWA/public/data/courses.js',
-	'/PWA/public/icons/icon-32.png',
-	'/PWA/public/icons/icon-64.png',
-	'/PWA/public/icons/icon-96.png',
-	'/PWA/public/icons/icon-128.png',
-	'/PWA/public/icons/icon-168.png',
-	'/PWA/public/icons/icon-192.png',
-	'/PWA/public/icons/icon-256.png',
-	'/PWA/public/icons/icon-512.png',
-	'/PWA/public/img/logo.png',
-];
-
-self.addEventListener('install', (e) => {
-	console.log('[Service Worker] : SW Installed');
+// Installing Service Worker
+self.addEventListener('install', function(e) {
+	console.log('[Service Worker] Install');
 	e.waitUntil(
-		caches
-			.open(cacheName)
-			.then(cache => {
-				console.log('[Service Worker] : SW Caching Files');
-				cache.addAll(cacheAssets);
-			}).then(() => self.skipWaiting())
+	  caches.open(cacheName).then(function(cache) {
+		console.log('[Service Worker] Caching all: app shell and content');
+		return cache.addAll(appShellFiles);
+	  })
 	);
-});
+  });
 
-self.addEventListener('activate', (e) => {
-	console.log('[Service Worker] : SW Activated');
-
-	e.waitUntil(
-		caches.keys().then(cacheNames => {
-			return Promise.all(
-				cacheNames.map(cache => {
-					if (cache !== cacheName) {
-						console.log("[Service Worker] : SW Deleted Old Cache");
-						return caches.delete(cache);
-					}
-				})
-			)
-		})
-	)
-});
+// Files to cache
+var cacheName = 'PWA';
+var appShellFiles = [
+  '/PWA/public/',
+  '/PWA/public/index.html',
+  '/PWA/public/style.css',
+  '/PWA/public/app.js',
+  '/PWA/public/favicon.ico',
+  '/PWA/public/manifest.webmanifest',
+  '/PWA/public/data/img',
+  '/PWA/public/data/courses.js',
+  '/PWA/public/icons/icon-32.png',
+  '/PWA/public/icons/icon-64.png',
+  '/PWA/public/icons/icon-96.png',
+  '/PWA/public/icons/icon-128.png',
+  '/PWA/public/icons/icon-168.png',
+  '/PWA/public/icons/icon-192.png',
+  '/PWA/public/icons/icon-256.png',
+  '/PWA/public/icons/icon-512.png',
+  '/PWA/public/img/logo.png',
+];
